64a91b86e1858e38370087a21014a5ac48086cfb
Kobby Pentangeli <kobbypentangeli@gmail.com>
2022-12-08 07:32:52 +0000

    Setup initial program flow and types

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+mod args;
+mod chunk;
+mod chunk_type;
+mod commands;
+mod img;
+
+pub type ProgramError = Box<dyn std::error::Error>;
+pub type ProgramResult<T> = std::result::Result<T, ProgramError>;
+
+fn main() -> ProgramResult<()> {
+    todo!()
 }
