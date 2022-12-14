1149cbc3bc37ee374514528105026fac600cbe26
Kobby Pentangeli <kobbypentangeli@gmail.com>
2022-12-14 11:54:44 +0000

    Implement command-line arg functionality

--- src/args/mod.rs
+++ src/args/mod.rs
@@ -9,19 +9,11 @@ pub struct App {
     /// Optional name of argument
     name: Option<String>,
 
-    /// Sets a custom config file
-    #[arg(short, long, value_name = "FILE")]
-    config: Option<PathBuf>,
-
-    /// Turn debugging information on
-    #[arg(short, long, action = clap::ArgAction::Count)]
-    debug: u8,
-
     #[command(subcommand)]
     command: Option<Commands>,
 }
 
-#[derive(Subcommand)]
+#[derive(Subcommand, Eq, PartialEq)]
 pub enum Commands {
     /// Add a secret message to a PNG file
     Encode(EncodeArgs),
@@ -36,10 +28,23 @@ pub enum Commands {
     Print(PrintArgs),
 }
 
-#[derive(Args)]
-pub struct EncodeArgs {}
+#[derive(Args, Debug, Eq, PartialEq)]
+pub struct EncodeArgs {
+    /// Path to the input PNG file
+    pub infile_path: PathBuf,
 
-#[derive(Args)]
+    /// Chunk type (like "ruSt")
+    #[arg(value_parser = ChunkType::from_str)]
+    pub chunk_type: ChunkType,
+
+    /// Your secret message
+    pub message: String,
+
+    /// Path to the output PNG file (optional)
+    pub outfile_path: Option<PathBuf>,
+}
+
+#[derive(Args, Debug, Eq, PartialEq)]
 pub struct DecodeArgs {
     /// Path to the PNG file
     pub file_path: PathBuf,
@@ -49,7 +54,7 @@ pub struct DecodeArgs {
     pub chunk_type: ChunkType,
 }
 
-#[derive(Args)]
+#[derive(Args, Debug, Eq, PartialEq)]
 pub struct RemoveArgs {
     /// Path to the PNG file
     pub file_path: PathBuf,
@@ -59,8 +64,102 @@ pub struct RemoveArgs {
     pub chunk_type: ChunkType,
 }
 
-#[derive(Args)]
+#[derive(Args, Debug, Eq, PartialEq)]
 pub struct PrintArgs {
     /// Path to the PNG file
     pub file_path: PathBuf,
 }
+
+// #[cfg(test)]
+// mod test {
+//     #[allow(unused_imports)]
+//     use super::*;
+
+//     #[test]
+//     pub fn test_encode() {
+//         let expected = EncodeArgs {
+//             infile_path: PathBuf::from("/a/b/c"),
+//             chunk_type: ChunkType::from_str("RuSt").unwrap(),
+//             message: "Cryptic imagico encoder".to_string(),
+//             outfile_path: None,
+//         };
+
+//         match &App::parse().command {
+//             Some(Commands::Encode(actual)) => {
+//                 assert_eq!(*actual, expected);
+//             }
+//             Some(_cmds) => {}
+//             None => {}
+//         }
+//     }
+
+//     #[test]
+//     pub fn test_encode_with_output_file() {
+//         let expected = EncodeArgs {
+//             infile_path: PathBuf::from("/a/b/c"),
+//             chunk_type: ChunkType::from_str("RuSt").unwrap(),
+//             message: "Cryptic imagico encoder".to_string(),
+//             outfile_path: Some(PathBuf::from("/output/file/path")),
+//         };
+
+//         match &App::parse().command {
+//             Some(Commands::Encode(actual)) => {
+//                 assert_eq!(*actual, expected);
+//             }
+//             Some(_cmds) => {}
+//             None => {}
+//         }
+//     }
+
+//     #[test]
+//     pub fn test_decode() {
+//         let expected = DecodeArgs {
+//             file_path: PathBuf::from("/a/b/c"),
+//             chunk_type: ChunkType::from_str("ImAg").unwrap(),
+//         };
+
+//         match &App::parse().command {
+//             Some(Commands::Decode(actual)) => {
+//                 assert_eq!(*actual, expected);
+//             }
+//             Some(_cmds) => {}
+//             None => {}
+//         }
+//     }
+
+//     #[test]
+//     pub fn test_remove() {
+//         let expected = RemoveArgs {
+//             file_path: PathBuf::from("/a/b/c"),
+//             chunk_type: ChunkType::from_str("imAG").unwrap(),
+//         };
+
+//         match &App::parse().command {
+//             Some(Commands::Remove(actual)) => {
+//                 assert_eq!(*actual, expected);
+//             }
+//             Some(_cmds) => {}
+//             None => {}
+//         }
+//     }
+
+//     #[test]
+//     pub fn test_print() {
+//         let expected = PrintArgs {
+//             file_path: PathBuf::from("/a/b/c"),
+//         };
+
+//         match &App::parse().command {
+//             Some(Commands::Print(actual)) => {
+//                 assert_eq!(*actual, expected);
+//             }
+//             Some(_cmds) => {}
+//             None => {}
+//         }
+//     }
+
+//     #[test]
+//     pub fn test_unknown_subcommand() {
+//         todo!()
+//     }
+// }
--- src/error.rs
+++ src/error.rs
@@ -1,6 +1,3 @@
-// /// Representing the various errors associated with this application.
-// pub type ProgramError = Box<dyn std::error::Error>;
-
 /// Representing returned results for various operations in this application.
 pub type ProgramResult<T> = std::result::Result<T, ProgramError>;
 
